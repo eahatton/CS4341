7f630d76194d4615b1507242169ecc552aa2f730
eahatton <62620957+eahatton@users.noreply.github.com>
2021-02-28 12:46:00 -0500

    setup test character to use different algorithms based on varient

--- Bomberman/group09/testcharacter.py
+++ Bomberman/group09/testcharacter.py
@@ -7,6 +7,31 @@ from colorama import Fore, Back
 
 class TestCharacter(CharacterEntity):
 
+    
+    def __init__(self, name, avatar, x, y, varient):
+        CharacterEntity.__init__(self, name, avatar, x, y)
+        self.varient = varient
+
+
     def do(self, wrld):
         # Your code here
+        if self.varient == 1:
+            self.astar()
+        elif self.varient == 2:
+            self.minimax()
+        elif self.varient >= 3:
+            self.expectimax()
         pass
+
+
+    def astar(self):
+        pass
+
+
+    def minimax(self):
+        pass
+
+    def expectimax(self):
+        pass
+
+
