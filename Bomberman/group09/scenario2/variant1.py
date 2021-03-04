5cb9d1ad556c62e01434187d395956672a64a1c4
eahatton <62620957+eahatton@users.noreply.github.com>
2021-03-03 19:47:25 -0500

    Updated Scenario 2 Variant 1 to fit the test character

--- Bomberman/group09/scenario2/variant1.py
+++ Bomberman/group09/scenario2/variant1.py
@@ -17,7 +17,8 @@ g = Game.fromfile('map.txt')
 # TODO Add your character
 g.add_character(TestCharacter("me", # name
                               "C",  # avatar
-                              0, 0  # position
+                              0, 0,  # position
+                                1
 ))
 
 # Run!
