60863639cedd076ac8c2d2b5d38f888838501e69
eahatton <62620957+eahatton@users.noreply.github.com>
2021-03-07 21:52:15 -0500

    smart astar

--- Bomberman/group09/scenario1/variant2.py
+++ Bomberman/group09/scenario1/variant2.py
@@ -11,9 +11,10 @@ from monsters.stupid_monster import StupidMonster
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
 from testcharacter import TestCharacter
+from interactivecharacter import InteractiveCharacter
 
 # Create the game
-random.seed(123) # TODO Change this if you want different random choices
+random.seed(125) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
 g.add_monster(StupidMonster("stupid", # name
                             "S",      # avatar
@@ -24,8 +25,15 @@ g.add_monster(StupidMonster("stupid", # name
 g.add_character(TestCharacter("me", # name
                               "C",  # avatar
                               0, 0, # position
-                              2     # Variant
+                              1     # Variant
 ))
+
+#g.add_character(InteractiveCharacter("me", "c", 0,0))
 
 # Run!
 g.go()
+
+events = g.world.events
+print(len(events))
+for e in events:
+    print(e.__str__())
